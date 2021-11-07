935da299b41ecc904014b1df732066e54cc1d369
addisudamena <addisudamena49@gmail.com>
2021-11-07 11:07:14 -0500

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Card from "./Card"
 import Favorite from "./Favorite"
+import Menu from "./Menu"
 
 function App() {
     return (
         <div>
+            <Menu>
             <Favorite/>
             <Card cardColor="red" height={200} width={200} />
             <Card cardColor="purple" />
